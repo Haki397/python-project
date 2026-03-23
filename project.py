1bd31a08211f51a3f2dba7b7eab61f6c7402bc76
Haki397 <hafraouihakim4@gmail.com>
2026-03-24 00:00:27 +0100

    25%

--- project.py
+++ project.py
@@ -6,15 +6,13 @@ class personne :
         self.telephone = telephone
         self.email = email
         self.type_personne = type_personne
-    
     def __str__(self):
         return f"[{self.id}] {self.prenom} {self.nom} - {self.type_personne}"
 class GestionImmoblier :
     def __init__(self, acheteure, vendeur, locataire):
-        self.achetuer = []
+        self.acheteur=[]
         self.vendeur = [] 
-        self.locataire = []
-    
+        self.locataire = [] 
     ###la partie de lajout des personnes
     def ajouter_vendeur(self, nom, prenom, telephone, email):
         id_personne = len(self.vendeur) + 1
@@ -23,9 +21,9 @@ class GestionImmoblier :
         print(f"Vendeur a etait ajoute avec succes : {vendeur}")
     
     def ajouter_acheteur(self, nom, prenom, telephone, email):
-        id_personne = len(self.achetuer) + 1
+        id_personne = len(self.acheteur) + 1
         acheteur = personne(id_personne, nom, prenom, telephone, email, "acheteur")
-        self.achetuer.append(acheteur)
+        self.acheteur.append(acheteur)
         print(f"Acheteur a etait ajoute avec succes : {acheteur}")
     
     def ajouter_locataire(self, nom, prenom, telephone, email):
@@ -39,7 +37,7 @@ class GestionImmoblier :
         for vendeur in self.vendeur:
             print(vendeur)
         print("\nListe des acheteurs :")
-        for acheteur in self.achetuer:
+        for acheteur in self.acheteur:
             print(acheteur)
         print("\nListe des locataires :")
         for locataire in self.locataire:
@@ -50,8 +48,6 @@ class GestionImmoblier :
             t, n, p, te = personne[index], personne[index].nom, personne[index].prenom, personne[index].telephone
             personne[index] = personne(t, nom or n, prenom or p, tel or te, personne[index].email, personne[index].type_personne)
             print(f"{type_recherche} modifié")
-        else:
-            print(" Personne non trouvée")
     ######## la partie de la suppression des personnes
     def suppr(self, c, i=None):  
         if i is None: 
@@ -59,3 +55,46 @@ class GestionImmoblier :
         else: 
             exec(f"{c}.pop({i})")
         print(f" {'tous' if i is None else c} supprimé(s)")
+class Appartement:
+    def __init__(self, id_appartement , adresse, superficie, prix):
+        self.id = id_appartement
+        self.adresse = adresse
+        self.superficie = superficie
+        self.prix = prix
+        self.photos_vendeur = []  # Liste des photos pour le vendeur avec coordonnées
+        self.photos_locataire = []  # Liste des photos pour le locataire avec coordonnées
+    
+    def ajouter_photo_vendeur(self, photo, coordonnees , prix, superficie,):
+        """Ajoute une photo pour le vendeur avec ses coordonnées."""
+        self.photos_vendeur.append({'photo': photo, 'coordonnees': coordonnees, 'prix': prix, 'superficie': superficie})
+    
+    def ajouter_photo_locataire(self, photo, coordonnees, prix, superficie):
+        """Ajoute une photo pour le locataire avec ses coordonnées."""
+        self.photos_locataire.append({'photo': photo, 'coordonnees': coordonnees, 'prix': prix, 'superficie': superficie})
+
+class Magasin:
+    def __init__(self, id_magasin):
+        self.id = id_magasin
+        self.photos_vendeur = []  # Liste des photos pour le vendeur avec coordonnées
+        self.photos_locataire = []  # Liste des photos pour le locataire avec coordonnées
+    
+    def ajouter_photo_vendeur(self, photo, coordonnees, prix, superficie):
+        """Ajoute une photo pour le vendeur avec ses coordonnées."""
+        self.photos_vendeur.append({'photo': photo, 'coordonnees': coordonnees, 'prix': prix, 'superficie': superficie})
+    
+    def ajouter_photo_locataire(self, photo, coordonnees, prix, superficie):
+        """Ajoute une photo pour le locataire avec ses coordonnées."""
+        self.photos_locataire.append({'photo': photo, 'coordonnees': coordonnees, 'prix': prix, 'superficie': superficie})
+class Terrain:
+    def __init__(self, id_terrain):
+        self.id = id_terrain
+        self.photos_vendeur = []  # Liste des photos pour le vendeur avec coordonnées
+        self.photos_locataire = []  # Liste des photos pour le locataire avec coordonnées
+    
+    def ajouter_photo_vendeur(self, photo, coordonnees, prix, superficie):
+        """Ajoute une photo pour le vendeur avec ses coordonnées."""
+        self.photos_vendeur.append({'photo': photo, 'coordonnees': coordonnees, 'prix': prix, 'superficie': superficie})
+    
+    def ajouter_photo_locataire(self, photo, coordonnees, prix, superficie):
+        """Ajoute une photo pour le locataire avec ses coordonnées."""
+        self.photos_locataire.append({'photo': photo, 'coordonnees': coordonnees, 'prix': prix, 'superficie': superficie})        
